532c2af581ef45852c168efff67798e45680c906
Sergey Rabchaev <rabchaev.s@gmail.com>
2023-01-11 11:40:23 +0100

    Fix entrypoint and configs

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -9,8 +9,15 @@ fi
 if [ ! -d "vendor" ]; then
   echo "composer install"
   composer install
+
+  while ! nc -z news-mysql 3306; do
+    echo "Waiting for MySQL ready..."
+    sleep 3
+  done
+
   echo "db migration"
   php artisan migrate
+
   echo "db seeding"
   php artisan db:seed
 fi
